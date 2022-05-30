0130f7669ba734c3846914ff4c80974e9417261b
Adriano Monteiro <adrianomonteiroweb@mail.com>
2022-05-30 16:33:52 -0300

    NEW: blocl class and get hash method.

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as crypto from "crypto";
+
 class Transaction {
   constructor(
     public amount: number,
@@ -7,5 +9,24 @@ class Transaction {
 
   toString() {
     return JSON.stringify(this);
+  }
+}
+
+class Block {
+  public numOnlyUsedOnce = Math.round(Math.random() * 999999999)
+  
+  constructor(
+    public prevHash: string,
+    public transaction: Transaction,
+    public ts: Date.now(),
+  ) {}
+
+  get hash() {
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash('SHA256');
+
+    hash.update(str).end();
+
+    return hash.digest('hex');
   }
 }
